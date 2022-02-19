1ed66d9da9214e41e373257b063d670d30dd9bbf
David Hurka <doxydoxy@mailbox.org>
2022-02-19 20:22:00 +0100

    Working on visual_line_number_displays.render_text_block()

---
+++ visual_line_number_displays/autotests/renderer_spec.lua
@@ -0,0 +1,113 @@
+-- SPDX-FileCopyrightText: 2022 David Hurka <doxydoxy@mailbox.org>
+--
+-- SPDX-License-Identifier: MIT OR LGPL-2.1-or-later
+
+-- Look for test subjects outside of autotests/
+package.path = "visual_line_number_displays/?.lua;" .. package.path
+
+-- See https://rubenwardy.com/minetest_modding_book/en/quality/unit_testing.html
+_G.visual_line_number_displays = {};
+
+-- Replacement for Font:render().
+local function render(_self, text, width, height, style)
+    local texture_string =  string.format("[combine:%ix%i:0,0=%s.png", width, height, text);
+    if style.color then
+        texture_string = texture_string .. "^[colorize:" .. style.color;
+    end
+    return texture_string;
+end
+
+visual_line_number_displays.font = {
+    render = render;
+}
+
+require("renderer");
+
+-- More readable replacement for texture_escape().
+-- Minetest documents texture grouping with parentheses,
+-- but the client source code doesn’t even care about it.
+local function texture_escape(input)
+    return "{" .. input .. "}";
+end
+
+visual_line_number_displays.texture_escape = texture_escape;
+
+describe("render_text_block", function()
+    local rtb = visual_line_number_displays.render_text_block;
+
+    it("renders plain text blocks", function()
+        local block = {
+            block = {
+                text = "A";
+                required_size = { width = 5, height = 8 };
+                text_size = { width = 5, height = 8 };
+            };
+            scale = 1;
+            position = { x = 0, y = 1 };
+            size = { width = 5, height = 8 };
+        };
+
+        assert.same(":0,1={[combine:5x8:0,0=A.png}", rtb(block, 1));
+    end);
+
+    it("renders superresolution text blocks", function()
+        local block = {
+            block = {
+                text = "A";
+                required_size = { width = 5, height = 8 };
+                text_size = { width = 5, height = 8 };
+            };
+            scale = 1;
+            position = { x = 0, y = 1 };
+            size = { width = 5, height = 8 };
+        };
+
+        assert.same(":0,2={[combine:5x8:0,0=A.png^[resize:10x16}", rtb(block, 2));
+    end);
+
+    it("renders scaled text blocks", function()
+        local block = {
+            block = {
+                text = "A";
+                required_size = { width = 5, height = 8 };
+                text_size = { width = 5, height = 8 };
+            };
+            scale = 0.5;
+            position = { x = 1, y = 2 };
+            size = { width = 3, height = 4 };
+        };
+
+        assert.same(":2,4={[combine:5x8:0,0=A.png}", rtb(block, 2));
+    end);
+
+    it("renders deplaced text blocks", function()
+        local block = {
+            block = {
+                text = "A";
+                required_size = { width = 9, height = 16 };
+                text_size = { width = 5, height = 8 };
+            };
+            scale = 1;
+            position = { x = 0, y = 1 };
+            size = { width = 9, height = 16 };
+        };
+
+        assert.same(":2,5={[combine:5x8:0,0=A.png}", rtb(block, 1));
+    end);
+
+    it("renders colored text blocks", function()
+        local block = {
+            block = {
+                text = "A";
+                required_size = { width = 5, height = 8 };
+                text_size = { width = 5, height = 8 };
+                text_color = "#00ff00";
+            };
+            scale = 1;
+            position = { x = 0, y = 1 };
+            size = { width = 5, height = 8 };
+        };
+
+        assert.same(":0,1={[combine:5x8:0,0=A.png^[colorize:#00ff00}", rtb(block, 1));
+    end);
+end);
---
+++ visual_line_number_displays/renderer.lua
@@ -0,0 +1,83 @@
+-- SPDX-FileCopyrightText: 2022 David Hurka <doxydoxy@mailbox.org>
+--
+-- SPDX-License-Identifier: MIT OR LGPL-2.1-or-later
+
+--! Escapes @p input for use in Minetest texture strings.
+--!
+--! This is shit, Minetest should just implement parentheses correctly,
+--! and everything would be cool.
+--! (Honestly, the whole texture string syntax is shit.)
+function visual_line_number_displays.texture_escape(input)
+    return string.gsub(input, "[:^]", "\\%1");
+end
+
+
+--! Makes a texture string depicting @p block.
+--!
+--! Result needs to be added to a @c combine modifier.
+--! It is returned with leading colon, and sufficiently escaped.
+--!
+--! @param block Layouted block; element from a blocks_layout table.
+--! @param sr_scale superresolution scaling factor for this display.
+function visual_line_number_displays.render_text_block(block, sr_scale)
+    -- Calculate block position and size in superresolution coordinates.
+    -- Because superresolution scales are integers, no rounding is needed here.
+    local block_position = {
+        x = block.position.x * sr_scale;
+        y = block.position.y * sr_scale;
+    };
+    local block_size = {
+        width = block.size.width * sr_scale;
+        height = block.size.height * sr_scale;
+    };
+
+    -- Calculate text position and size in block coordinates.
+    local block_text_position = {
+        x = (block.block.required_size.width - block.block.text_size.width) * 0.5;
+        y = (block.block.required_size.height - block.block.text_size.height) * 0.5;
+    };
+
+    -- Calculate text position and size in layout coordinates.
+    local layout_text_position = {
+        x = block.position.x + block_text_position.x * block.scale;
+        y = block.position.y + block_text_position.y * block.scale;
+    };
+    local layout_text_size = {
+        width = block.block.text_size.width * block.scale;
+        height = block.block.text_size.height * block.scale;
+    };
+
+    -- Calculate text position and size in superresolution coordinates.
+    -- Exactly here we need rounding,
+    -- because block scales are not integers and should line up in superresolution.
+    local text_position = {
+        x = math.floor(layout_text_position.x * sr_scale);
+        y = math.floor(layout_text_position.y * sr_scale);
+    };
+    local text_size = {
+        width = math.floor(layout_text_size.width * sr_scale);
+        height = math.floor(layout_text_size.height * sr_scale);
+    };
+
+    -- Render text.
+    local text_color = block.block.text_color;
+    if text_color == "black" or text_color == "#000000" then
+        text_color = nil;
+    end
+    local text_style = {
+        halign = "center";
+        color = text_color;
+    };
+    local text_texture = visual_line_number_displays.font:render(block.block.text, block.block.text_size.width, block.block.text_size.height, text_style);
+
+    local total_text_scale = block.scale * sr_scale;
+    if total_text_scale ~= 1 then
+        text_texture = text_texture .. string.format("^[resize:%ix%i", text_size.width, text_size.height);
+    end
+
+    text_texture = visual_line_number_displays.texture_escape(text_texture);
+
+    text_texture = string.format(":%i,%i=", text_position.x, text_position.y) .. text_texture;
+
+    return text_texture;
+end
